e6d3dab63c3e6014f6603117b26e9daa3f0af2c7
tpettry <tpettry5449@gmail.com>
2020-09-20 08:12:12 -0700

    Completed objects lesson

---
+++ Javascript/objects/faceBook_excercise/script.js
@@ -0,0 +1,32 @@
+var database = [
+  {
+    username: "andrei",
+    password: "supersecret"
+  }
+];
+
+var newsFeed = [
+  {
+    username: "Bobby",
+    timeline: "So tired from all that learning"
+  },
+  {
+    username: "Sally",
+    timeline: "Javascript is sooooo cooool!"
+  }
+];
+
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
+
+
+function signIn(user, pass) {
+  if (user === database[0].username &&
+      pass === database[0].password) {
+        console.log(newsFeed);
+  }else {
+    alert("Sorry wrong username and or password");
+  }
+}
+
+signIn(userNamePrompt, passwordPrompt);
